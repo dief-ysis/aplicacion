40aca64efecaf732b1302543b8396a15b25fe526
Plaza Vespucio <plazavespucio@PVAWSB03LC0122.duoc.acad>
2025-05-14 21:49:55 -0400

    conexion bd

--- src/main/java/com/example/demo/model/Libro.java
+++ src/main/java/com/example/demo/model/Libro.java
@@ -1,18 +1,57 @@
 package com.example.demo.model;
 
+import java.time.LocalDate;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
+//@Data
+//@AllArgsConstructor
+//@NoArgsConstructor
 
+
+//public class Libro {
+//    private int id;
+//    private String isbn;
+//    private String titulo;
+//    private String editorial;
+//    private int fechaPublicacion;
+//    private String autor;
+//}
+
+
+@Entity
+@Table(name = "libro")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Libro {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+
+    @Column(name = "isbn", length = 255, unique = true, nullable = false)
     private String isbn;
-    private String titulo;
-    private String editorial;
-    private int fechaPublicacion;
-    private String autor;
+
+    @Column(name = "title", length = 255, unique = true, nullable = false)
+    private String title;
+
+    @Column(name = "author", length = 255, unique = true, nullable = false)
+    private String author;
+
+    @Column(name = "publisher", length = 255, unique = true, nullable = false)
+    private String publisher;
+
+    // @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "publication_date", nullable = false)
+    private LocalDate publication_date;
+
 }
